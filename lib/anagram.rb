a23d63536747525c07cb017397930abf4e3144e0
Shelby Lee <slee337@uw.edu>
2022-09-20 10:23:07 -0600

    commit tree fbf62becaf75a6f30bff9d583de034478b2495f7

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(a_o_w)
+        a_o_w.select {|o_w| o_w.chars.sort == word.chars.sort}
+    end
+
+end
+
+# binding.pry
